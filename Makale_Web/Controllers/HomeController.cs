483aa7d0ac91f1dddf99b7f287c1bb6a27c8cdc0
beytullahbyram <beytullah_bayram67@hotmail.com>
2022-12-15 10:43:16 +0300

    .

--- Makale_Web/Controllers/HomeController.cs
+++ Makale_Web/Controllers/HomeController.cs
@@ -39,5 +39,19 @@ namespace Makale_Web.Controllers
         {
             return View("Index",makaleYonet.MakaleListele().OrderByDescending(x=>x.BegeniSayisi).ToList());
         }
+
+
+        public ActionResult Hakkımızda()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Register()
+        {
+            return View();
+        }
     }
 }
